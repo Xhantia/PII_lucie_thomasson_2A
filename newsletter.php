a3cc09f04c94c11566b4a10b5fa1cc7038ee7c66
Xhantia <44496631+Xhantia@users.noreply.github.com>
2021-04-04 16:14:57 +0200

    newsletter ok

--- newsletter.php
+++ newsletter.php
@@ -1,6 +1,7 @@
 
 <?php session_start();
     require_once "includes/functions.php";
+    $status = "";
 
     if( isset($_POST['email']) && isset($_POST['pseudo']) ){
 
@@ -10,19 +11,19 @@
         // echo $email;
         // echo $pseudo;
 
-        $requete = getDb() -> exec ('INSERT INTO newsletter (pseudo_abonne, mail_abonne) VALUES ('$pseudo', '$email')');
-        echo $requete;
+        $requete = getDb() -> prepare ('INSERT INTO newsletter (pseudo_abonne, mail_abonne) VALUES (:pseudo_abonne, :mail_abonne)');
+        // echo $requete;
 
-        // $requete -> bindValue ('mail_abonne', $email, PDO :: PARAM_STR);
-        // $requete -> bindValue ('pseudo_abonne', $pseudo, PDO :: PARAM_STR);
-	    // $requete -> execute ();
+        $requete -> bindValue ('pseudo_abonne', $pseudo, PDO :: PARAM_STR);
+        $requete -> bindValue ('mail_abonne', $email, PDO :: PARAM_STR);
+	    $requete -> execute ();
 
-
-
-       
+        $status = "Success";
+        echo $status;       
     }
 	else {
-		echo "Failed";
+        $status = "Failed";
+		echo $status;
 	}
 ?>
 
